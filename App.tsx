c549bd2ee9ad6d928ed083955de2537f818a24ee
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-06-05 09:40:41 -0700

    setting up typescript, vite, and react

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import Home from "./src/Home";
+import SignUp from "./src/SignUp";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <SignUp />
+    </>
+  );
+}
---
+++ src/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Tyler and Lesley</p>
+    </div>
+  );
+}
