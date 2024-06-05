96a575f7270996c677b3c24b5be230a40032af66
lesleytomosada <tomosadalesley@gmail.com>
2024-06-05 10:15:11 -0700

    installed express, postgres, jest, linters,  and setup browser router and routes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import Home from "./src/Home";
 import SignUp from "./src/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Home />
-      <SignUp />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
