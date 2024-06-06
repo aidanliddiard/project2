5933ef8cffb289473d0652aa7fcca352145b54dd
lesleytomosada <tomosadalesley@gmail.com>
2024-06-06 14:44:07 -0700

    consolidated get endpoints into one using join and Tyler's get all and create vacation endpoints working

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const itineraryQueries = require("./src/itineraryQueries.js");
+const vacationQueries = require("./src/vacationQueries.js");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,9 +14,12 @@ app.use(
   })
 );
 
+
+app.get("/api/vacations", vacationQueries.getVacations);
+app.post("/api/vacations/", vacationQueries.createVacation);
+
+
 app.get("/api/vacation/itinerary/:id", itineraryQueries.getItinerary);
-app.get("/api/category", itineraryQueries.getCategory);
-app.get("/api/time", itineraryQueries.getTime);
 app.post("/api/vacation/itinerary/", itineraryQueries.createItineraryItem);
 app.put("/api/vacation/itinerary/item/:id", itineraryQueries.updateItineraryItem);
 app.delete("/api/vacation/itinerary/item/:id", itineraryQueries.deleteItineraryItem);
@@ -23,4 +27,5 @@ app.delete("/api/vacation/itinerary/item/:id", itineraryQueries.deleteItineraryI
 app.listen(port, () => {
   console.log(`App running on port http://localhost:${port}`);
   console.log(itineraryQueries);
+  console.log(vacationQueries);
 });
--- backend/src/itineraryQueries.js
+++ backend/src/itineraryQueries.js
@@ -1,52 +1,85 @@
-const pool = require('./server.js');
+const pool = require("./server.js");
 
 const createItineraryItem = (request, response) => {
-  const { name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id } = request.body;
-  pool.query('INSERT INTO itinerary (name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id ], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(201).send(`Itinerary added with ID: ${results.insertId}`);
-  });
-}
-
-const getCategory = (request, response) => {
-  pool.query('SELECT * from CATEGORY', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  })
-} 
-
-const getTime = (request, response) => {
-    console.log(pool)
-    pool.query('SELECT * FROM time', (error, results) => {
+  const {
+    name,
+    category_id,
+    price,
+    address,
+    description,
+    start_date,
+    end_date,
+    time_id,
+    website,
+    vacation_id,
+  } = request.body;
+  pool.query(
+    "INSERT INTO itinerary (name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+    [
+      name,
+      category_id,
+      price,
+      address,
+      description,
+      start_date,
+      end_date,
+      time_id,
+      website,
+      vacation_id,
+    ],
+    (error, results) => {
       if (error) {
         throw error;
       }
-      console.log(results.rows)
-      response.status(200).json(results.rows);
-    });
-  } 
+      response.status(201).send(`Itinerary added with ID: ${results.insertId}`);
+    }
+  );
+};
 
 const getItinerary = (request, response) => {
   const id = request.params.id;
-  pool.query('SELECT * FROM itinerary WHERE vacation_id=$1', [id], (error, results) => {
-    if (error) {
-      throw error;
+  pool.query(
+    "SELECT name, price, address, description, start_date, end_date, website, time.time, category.type FROM itinerary LEFT JOIN time on time.id = itinerary.time_id LEFT JOIN category on category.id = itinerary.category_id WHERE vacation_id=$1;",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
-    response.status(200).json(results.rows);
-  });
-}
+  );
+};
 
 const updateItineraryItem = (request, response) => {
   const id = parseInt(request.params.id);
-  console.log(request.body)
-  const { name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id } = request.body;
+  console.log(request.body);
+  const {
+    name,
+    category_id,
+    price,
+    address,
+    description,
+    start_date,
+    end_date,
+    time_id,
+    website,
+    vacation_id,
+  } = request.body;
   pool.query(
-    'UPDATE itinerary SET name = $1, category_id = $2, price = $3, address = $4, description = $5, start_date = $6, end_date = $7, time_id = $8, website = $9, vacation_id = $10 WHERE id = $11',
-    [name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id, id],
+    "UPDATE itinerary SET name = $1, category_id = $2, price = $3, address = $4, description = $5, start_date = $6, end_date = $7, time_id = $8, website = $9, vacation_id = $10 WHERE id = $11",
+    [
+      name,
+      category_id,
+      price,
+      address,
+      description,
+      start_date,
+      end_date,
+      time_id,
+      website,
+      vacation_id,
+      id,
+    ],
     (error, results) => {
       if (error) {
         throw error;
@@ -54,23 +87,23 @@ const updateItineraryItem = (request, response) => {
       response.status(200).send(`Itinerary modified with ID: ${id}`);
     }
   );
-}
+};
 
 const deleteItineraryItem = (request, response) => {
   const id = parseInt(request.params.id);
-  pool.query('DELETE FROM itinerary WHERE id = $1', [id], (error, results) => {
+  pool.query("DELETE FROM itinerary WHERE id = $1", [id], (error, results) => {
     if (error) {
       throw error;
     }
     response.status(200).send(`Itinerary deleted with ID: ${id}`);
   });
-}
+};
 
-  module.exports = {
-    createItineraryItem,
-    getCategory,
-    getItinerary,
-    getTime,
-    updateItineraryItem,
-    deleteItineraryItem
-  }
+module.exports = {
+  createItineraryItem,
+  getCategory,
+  getItinerary,
+  getTime,
+  updateItineraryItem,
+  deleteItineraryItem,
+};
