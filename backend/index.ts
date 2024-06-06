221237ebaea2ed2f193f660996634ac9c3ff60f0
lesleytomosada <tomosadalesley@gmail.com>
2024-06-06 13:31:26 -0700

    completed endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -13,8 +13,12 @@ app.use(
   })
 );
 
+app.get("/api/vacation/itinerary/:id", itineraryQueries.getItinerary);
+app.get("/api/category", itineraryQueries.getCategory);
 app.get("/api/time", itineraryQueries.getTime);
-app.post("/api/itinerary", itineraryQueries.createItineraryItem);
+app.post("/api/vacation/itinerary/", itineraryQueries.createItineraryItem);
+app.put("/api/vacation/itinerary/item/:id", itineraryQueries.updateItineraryItem);
+app.delete("/api/vacation/itinerary/item/:id", itineraryQueries.deleteItineraryItem);
 
 app.listen(port, () => {
   console.log(`App running on port http://localhost:${port}`);
--- backend/src/itineraryQueries.js
+++ backend/src/itineraryQueries.js
@@ -1,15 +1,5 @@
 const pool = require('./server.js');
 
-const getItinerary = (request, response) => {
-  const id = request.params.id;
-  pool.query('SELECT * FROM itinerary WHERE id=$1', [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-}
-
 const createItineraryItem = (request, response) => {
   const { name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id } = request.body;
   pool.query('INSERT INTO itinerary (name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id ], (error, results) => {
@@ -20,7 +10,16 @@ const createItineraryItem = (request, response) => {
   });
 }
 
-const getTime = (response) => {
+const getCategory = (request, response) => {
+  pool.query('SELECT * from CATEGORY', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  })
+} 
+
+const getTime = (request, response) => {
     console.log(pool)
     pool.query('SELECT * FROM time', (error, results) => {
       if (error) {
@@ -30,9 +29,48 @@ const getTime = (response) => {
       response.status(200).json(results.rows);
     });
   } 
-  
+
+const getItinerary = (request, response) => {
+  const id = request.params.id;
+  pool.query('SELECT * FROM itinerary WHERE vacation_id=$1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+}
+
+const updateItineraryItem = (request, response) => {
+  const id = parseInt(request.params.id);
+  console.log(request.body)
+  const { name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id } = request.body;
+  pool.query(
+    'UPDATE itinerary SET name = $1, category_id = $2, price = $3, address = $4, description = $5, start_date = $6, end_date = $7, time_id = $8, website = $9, vacation_id = $10 WHERE id = $11',
+    [name, category_id, price, address, description, start_date, end_date, time_id, website, vacation_id, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Itinerary modified with ID: ${id}`);
+    }
+  );
+}
+
+const deleteItineraryItem = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query('DELETE FROM itinerary WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Itinerary deleted with ID: ${id}`);
+  });
+}
+
   module.exports = {
     createItineraryItem,
+    getCategory,
     getItinerary,
-    getTime
+    getTime,
+    updateItineraryItem,
+    deleteItineraryItem
   }
