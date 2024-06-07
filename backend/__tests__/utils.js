8490816a09c398756eadcd9dce332fe5d0934da7
lesleytomosada <tomosadalesley@gmail.com>
2024-06-07 13:00:48 -0700

    moved signInAndSignUp to utils

--- backend/__tests__/utils.js
+++ backend/__tests__/utils.js
@@ -3,6 +3,25 @@ const path = require("node:path");
 const pool = require("../src/server.js");
 const sql = readFileSync(path.join(__dirname, "../src/setup.sql"), "utf-8");
 
+const request = require("supertest");
+const { app } = require("../index.ts");
+
+const mockUser = {
+  name: "Test",
+  email: "test@testing.com",
+  password: "password",
+};
+
+const signInAndSignUp = async () => {
+  try {
+    const agent = await request.agent(app);
+    const user = await agent.post("/api/auth/users").send(mockUser);
+    return [agent, user];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function setupDb() {
   return pool.query(sql);
 }
@@ -15,4 +34,6 @@ afterAll(closeAll);
 
 module.exports = {
   setupDb,
+  mockUser,
+  signInAndSignUp,
 };
