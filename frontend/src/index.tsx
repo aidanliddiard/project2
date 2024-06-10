0c18bed48fc82ed36a2778720720c7ddb1bb170e
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-06-10 08:34:41 -0700

    setting up frontend routes and context for auth

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "../../App";
+import { UserProvider } from "./context/userContext";
 
 const rootElement = document.getElementById("root");
-ReactDOM.createRoot(rootElement).render(<App />);
+ReactDOM.createRoot(rootElement).render(
+  <UserProvider>
+    <App />
+  </UserProvider>
+);
