1f9360e8dc59c8da2980175e6056aaa6989551a9
Tyler Byrd <tyler.byrd@cvent.com>
2024-06-11 16:18:16 -0400

    Updated the card formatting

--- frontend/src/components/Vacations.tsx
+++ frontend/src/components/Vacations.tsx
@@ -12,10 +12,9 @@ interface VacationObject {
   user_id: Number;
 }
 
-// Define the formatDate function
-function formatDate(dateString) {
+function formatDate(dateString: string): string {
   const date = new Date(dateString);
-  const options = { month: 'long', day: 'numeric' };
+  const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
   return date.toLocaleDateString(undefined, options);
 }
 
@@ -34,36 +33,25 @@ export default function Vacations() {
     <>
       <NavBar />
       <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-            Wanderlust
-          </a>
-          <div>
+      <div className="flex flex-col items-center justify-center px-6 py-8">
+      <div className="grid grid-cols-3 gap-3 pt-8">
             {vacation.map((vacation: VacationObject, index: number) => (
-              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                  <img className="rounded-t-lg" src="paris.jpg" alt="" />
-                </a>
-                <div className="p-5">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 card grid-cols-3 gap-3">
                   <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{vacation.city}</h5>
+                    <img className="rounded-t-lg" src="frontend/src/components/paris.jpg" alt="paris" />
                   </a>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{formatDate(vacation.start_date)} - {formatDate(vacation.end_date)}</p>
-                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    View details
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                  </a>
-                </div>
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{vacation.city}</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{formatDate(vacation.start_date)} - {formatDate(vacation.end_date)}</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      View details
+                      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                      </svg>
+                    </a>
+                  </div>
               </div>
             ))}
           </div>
