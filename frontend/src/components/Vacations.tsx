d81b5c79721d5f13ff507c74ee2eca3db5d8d532
Tyler Byrd <tyler.byrd@cvent.com>
2024-06-11 15:33:04 -0400

    Populated with the cards with the correct properties

--- frontend/src/components/Vacations.tsx
+++ frontend/src/components/Vacations.tsx
@@ -1,38 +1,74 @@
 import { useEffect, useState } from "react";
 import { fetchVacations } from "../services/vacations";
+import NavBar from "./NavBar";
 
 interface VacationObject {
-    id: number;
-    city: string;
-    country: string;
-    description?: string;
-    start_date: string;
-    end_date: string; 
-    user_id: Number;
+  id: number;
+  city: string;
+  country: string;
+  description?: string;
+  start_date: string;
+  end_date: string;
+  user_id: Number;
+}
+
+// Define the formatDate function
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { month: 'long', day: 'numeric' };
+  return date.toLocaleDateString(undefined, options);
 }
 
 export default function Vacations() {
-    const [vacation, setVacation] = useState<VacationObject[]>([]);
-  
-    useEffect(() => {
-      const fetchVacationData = async () => {
-        const resp = await fetchVacations();
-        setVacation(resp);
-      };
-      fetchVacationData();
-    }, []);
-  
-    return (
-      <div>
-        {vacation.map((vacation: VacationObject, index: number) => (
-          <div key={index}>
-            <p>{vacation.city}</p>
-            <p>{vacation.country}</p>
-            <p>{vacation.description}</p>
-            <p>{vacation.start_date}</p>
-            <p>{vacation.end_date}</p>
+  const [vacation, setVacation] = useState<VacationObject[]>([]);
+
+  useEffect(() => {
+    const fetchVacationData = async () => {
+      const resp = await fetchVacations();
+      setVacation(resp);
+    };
+    fetchVacationData();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              className="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            />
+            Wanderlust
+          </a>
+          <div>
+            {vacation.map((vacation: VacationObject, index: number) => (
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img className="rounded-t-lg" src="paris.jpg" alt="" />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{vacation.city}</h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{formatDate(vacation.start_date)} - {formatDate(vacation.end_date)}</p>
+                  <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    View details
+                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </>
+  );
+}
--- frontend/src/services/vacations.ts
+++ frontend/src/services/vacations.ts
@@ -1,12 +1,3 @@
-export async function fetchTime() {
-  const response = await fetch("http://localhost:3000/api/time");
-
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-
-
 export async function fetchVacations() {
   const response = await fetch("http://localhost:3000/api/vacations");
 
