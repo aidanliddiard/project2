f7699fb044942bec2d380ec3f1b444a30abf1584
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-06-13 09:38:08 -0700

    setup private routes

---
+++ frontend/src/components/PrivateRoutes.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserContext } from "../context/userContext";
+
+export default function PrivateRoutes() {
+  const { currentUser } = useUserContext();
+
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+}
