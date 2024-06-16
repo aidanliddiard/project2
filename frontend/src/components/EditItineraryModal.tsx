ffd7396166e57229313b59857b156dc073fbfe9b
lesleytomosada <tomosadalesley@gmail.com>
2024-06-15 17:35:30 -0700

    consistent spacing added on edit form

--- frontend/src/components/EditItineraryModal.tsx
+++ frontend/src/components/EditItineraryModal.tsx
@@ -84,8 +84,7 @@ const EditModal: React.FC<EditModalProps> = ({ onClose, id, vacationId }) => {
           <div className="relative">
             <div className="flex flex-col items-center justify-center px-3 py-4 mx-auto lg:py-0">
               <div className="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0 ">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                  <div>
+              <div className="px-4 pt-4 space-y-4 md:space-y-4 sm:px-8 sm:pt-8 sm:p-4">                  <div>
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                       Update Itinerary Item
                     </h1>
