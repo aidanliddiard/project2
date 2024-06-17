736b18df384a20f29ceec3133eaadf76416185ab
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-06-17 11:04:16 -0700

    fixing login padding

--- frontend/src/views/LogIn.tsx
+++ frontend/src/views/LogIn.tsx
@@ -31,11 +31,11 @@ export default function LogIn() {
   return (
     <>
       <NavBar />
-      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen pt-10 pb-8">
         <div className={`${toastOpen ? "" : "hidden"}`}>
           <ToastError errorMessage={errorMessage} setToastOpen={setToastOpen} />
         </div>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center px-6 mx-auto md:h-screen pt-10">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
